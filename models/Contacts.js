40ace2b91ffca0107fa9b0f225b2e48be06f6c7c
kamrulcoder <kamrulcoder@gmail.com>
2021-11-23 14:32:15 +0600

    all file includes

---
+++ models/Contacts.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const ContactSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        phone: { type: Number, required: true },
+        description: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+
+
+
+module.exports = model("Contact", ContactSchema);
---
+++ routes/contacts.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const Contact = require("../models/Contacts")
+
+router.post("/", async (req, res) => {
+    const newContact = new Contact(req.body);
+
+    try {
+        const savedContact = await newContact.save();
+        res.status(200).json(savedContact);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// All Contacts 
+router.get("/", async (req, res) => {
+    try {
+        const contacts = await Contact.find();
+        res.status(200).json(contacts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router
